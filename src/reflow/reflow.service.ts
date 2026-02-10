6640132d7fe984dc69cbd3107cbf2ab8a5e6e44f
Alex <aaahandour@gmail.com>
2026-02-09 19:00:25 -0500

    implement Kahn's algorithm for topological sorting of work orders

--- src/reflow/reflow.service.ts
+++ src/reflow/reflow.service.ts
@@ -4,12 +4,15 @@ import {
     Result,
     Change
 } from "./types";
+
 import {
     DateTime
 } from "luxon";
+
 import {
     DateUtils
 } from "../utils/date.utils";
+import { ConstraintChecker } from "./constraint.checker";
 
 export class ReflowService {
 
@@ -32,7 +35,7 @@ export class ReflowService {
             // Schedule the next order in line, passing all work centers and the current time pointer array
             let [scheduledWorkOrder, change] = this.scheduleWorkOrder(wo, workCenters, timePointers)
 
-            // Save the resulting work order no matter what
+            // Save the resulting work order
             scheduledWorkOrders.push(scheduledWorkOrder)
 
             // We only document changes if a work order had its time changed by delays or maintenance
@@ -50,22 +53,68 @@ export class ReflowService {
     sortWorkOrders(
         workOrders: WorkOrder[]
     ): WorkOrder[] {
-        let childWorkOrders = new Map<string, string[]>();
-        let numberOfDependencies = new Map<string, number>();
 
-        // Initialize empty children arrays and dependency depths
+        // Start off with a startTime based sort before handling dependencies with Kahn's algorithm.
+        workOrders.sort(this.sortByPlannedStartTimeThenId)
+
+        // Create a quick lookup map to get work orders by their Id
+        const idLookup = new Map<string, WorkOrder>();
+        for (const wo of workOrders) {
+            idLookup.set(wo.docId, wo);
+        }
+
+        // Initialize empty children and inDegree maps to build our DAG
+        let childWorkOrders = new Map<string, string[]>();
+        let inDegree = new Map<string, number>();
         for (const wo of workOrders) {
             childWorkOrders.set(wo.docId, [])
-            numberOfDependencies.set(wo.docId, 0)
+            inDegree.set(wo.docId, 0)
         }
 
         // For each work order, iterate through the parents it depends on 
         for (const wo of workOrders) {
             for (const parentId of wo.data.dependsOnWorkOrderIds) {
+                //Creates a map that connects parent work order to an array of its child work orders
                 childWorkOrders.get(parentId)?.push(wo.docId)
-                numberOfDependencies.set(wo.docId, (numberOfDependencies.get(wo.docId) ?? 0) + 1)
+
+                //Checks the number of incoming vertices (work orders it depends on) per work order
+                //For every work order that has the child as a parent, inDegree will increment
+                inDegree.set(wo.docId, (inDegree.get(wo.docId) ?? 0) + 1)
             }
         }
+
+        // Initialize DAG Queue with all non dependent work orders
+        const queue: WorkOrder[] = []
+        for (const wo of workOrders) {
+            if (inDegree.get(wo.docId) === 0) {
+                queue.push(wo);
+            }
+        }
+
+        // Instantiate our finally sorted work order list
+        let topologicallySortedWorkOrders: WorkOrder[] = []
+
+        // Create a revolving queue that adds work orders as dependencies are resolved
+        while(queue.length > 0) {
+
+            // Pop the next work order with resolved dependencies and add it to the sorted list
+            const currentWorkOrder = queue.shift()!;
+            topologicallySortedWorkOrders.push(currentWorkOrder);
+
+            // Iterate through current worker's children and reduce inDegree by 1
+            for(const childId of childWorkOrders.get(currentWorkOrder.docId)!){
+                inDegree.set(childId, inDegree.get(childId)! - 1);
+
+                // Add children to the queue once we resolve dependencies (inDegree = 0)
+                if (inDegree.get(childId) == 0) queue.push(idLookup.get(childId)!);
+            }
+        }
+
+        // Quick check for cyclic dependencies by ensuring the number of work orders hasn't changed
+        if(ConstraintChecker.checkForCyclicDependencies(workOrders, topologicallySortedWorkOrders)) {
+            throw new Error("Cyclic dependency detected. Cannot proceed with scheduling")
+        }
+        return topologicallySortedWorkOrders;
     }
 
     scheduleWorkOrder(
@@ -74,6 +123,18 @@ export class ReflowService {
         timePointers: Map<string, DateTime>
     ): [WorkOrder, Change] {
 
+    }
+
+    // Helper function to have lower startTime work orders get processed first with Id as a tiebreaker
+    sortByPlannedStartTimeThenId(a: WorkOrder, b: WorkOrder) {
+        const aDateTime = DateUtils.stringToDateTime(a.data.startDate)
+        const bDateTime = DateUtils.stringToDateTime(b.data.startDate)
+        const diff = aDateTime.toMillis() - bDateTime.toMillis()
+        if(diff !== 0) {
+            return diff
+        } else {
+            return a.docId.localeCompare(b.docId);
+        }
     }
 
 }
