b998dec2287a0e334e4b6c6daeab032c59ea5689
Alex <aaahandour@gmail.com>
2026-02-13 17:38:39 -0500

    fix timezone issue in date utils

--- src/utils/date.utils.ts
+++ src/utils/date.utils.ts
@@ -4,8 +4,12 @@ import { WorkCenter } from "../reflow/types";
 export class DateUtils {
 
     static stringToDateTime(dateStr: string): DateTime {
-        return DateTime.fromISO(dateStr);
+        return DateTime.fromISO(dateStr, { zone: 'utc' });
     }
+
+    static stringUTCtoISO(dateString: string): string {
+    return this.stringToDateTime(dateString).toUTC().toISO()!; // always ends with 'Z'
+}
 
     static initializeTimePointers(workCenters: WorkCenter[], startTime: string): Map<string, DateTime> {
         let startDateTime = this.stringToDateTime(startTime);
